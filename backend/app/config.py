308a8caae59cac2f4337bf873b040da813923567
Nikita <nikitooo111333222@gmail.com>
2024-11-05 22:13:31 +0300

    add settings for database

---
+++ backend/app/config.py
@@ -0,0 +1,22 @@
+from pydantic_settings import BaseSettings
+from pydantic import ConfigDict, model_validator, Field
+
+
+class Settings(BaseSettings):
+    model_config = ConfigDict(env_file=".env")
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+    
+    @property
+    def DB_URL(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    
+    
+    SEKRET_KEY: str
+    ALGORITHM: str
+    
+
+settings = Settings()
